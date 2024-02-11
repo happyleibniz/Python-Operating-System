e5cf7f37ecf67cf64fc189c0833ec8ff0e854696
happyleibniz <121920485+happyleibniz@users.noreply.github.com>
2024-02-11 22:12:33 +0800

    Delete USING pyglet directory

--- USING pyglet/V0.114514.1919810/Disk/__system/__internal/freezed_variables.py
+++
@@ -1,8 +0,0 @@
-import platform
-vm_info = "Python virtual machine version 1.0"
-booter= "Python Legends" # like american megatrends
-os_name = platform.system()
-os_version = platform.version()
-cpu_info = platform.processor()
-ram_info = platform.machine()
-
--- USING pyglet/v0.114516.1919810/Disk/__system/__internal/font_get.py
+++
@@ -1,7 +0,0 @@
-import pyglet
-
-
-def GetFont(FontName, FontSize):
-    pyglet.resource.add_font(FontName)
-    font = pyglet.font.load(FontName, int(FontSize))
-    return font
