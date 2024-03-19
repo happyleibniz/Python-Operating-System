15dccee3d907ada649763b5c9aeb926580ba010a
happyleibniz-archive <happyleibniz@outlook.com>
2024-03-19 12:57:19 +0800

    pyglet3192024

---
+++ pyglet3192024/core/utils/freezed_variables.py
@@ -0,0 +1,8 @@
+import platform
+
+vm_info = "Python virtual machine version 1.0"
+booter = "Python Legends"  # like american mega trends
+os_name = platform.system()
+os_version = platform.version()
+cpu_info = platform.processor()
+ram_info = platform.machine()
